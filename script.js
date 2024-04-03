36412e3e3e7ca5e05379f4bcd2cf9157b73c11be
Leonardo Cunha <lleon.sc27@gmail.com>
2024-04-03 12:04:30 -0300

    Ajuste na organização do código

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const DOLAR = 5.08
 alert("Olá!\n\nBem-vindo ao Conversor de Moedas!\n\nEste conversor permite a conversão entre dólares e reais. Atualmente, 1 dólar equivale a aproximadamente 5,08 reais.")
 var valor = prompt("Digite o valor para ser convertido: \n\nSiga o seguinte formato: R$10,00 ou $10,00")
+
+const DOLAR = 5.08
 
 function conversao() {
     var numero = parseFloat(valor.replace(/[^\d.,]/g, '').replace(',', '.'));
